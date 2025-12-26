ff9fd738a76526d47bc5fd44e17b696eca5cd5bc
Divine Chidozie <igwedivine92@gmail.com>
2025-12-26 02:54:01 +0100

    Updating switch between paragraphs

--- script.js
+++ script.js
@@ -35,24 +35,49 @@ downloadButton.addEventListener("click", function (event) {
   alert("Download CV button clicked");
 });
 
-// Enable DarkMode and Check Localstorage for saved data
-const toggleButton = document.getElementById("toggle-btn");
+// ===== Dark Mode =====
+const toggleButton = document.querySelector("#toggle-btn");
+
+// Apply saved theme on page load
+if (localStorage.getItem("darkMode") === "enabled") {
+  document.body.classList.toggle("dark-background");
+}
+
+// Toggle dark mode on button click
 toggleButton.addEventListener("click", () => {
-  // Check localstorage on page load
-  if (localStorage.getItem("darkMode") === "enabled") {
-    document.body.classList.toggle("dark-background");
-  }
+  document.body.classList.toggle("dark-background");
+});
 
-  toggleButton.addEventListener("click", () => {
-    document.body.classList.toggle("dark-background");
-  });
+// Save the preference
+if (document.body.classList.contains("dark-background")) {
+  localStorage.setItem("darkMode", "enabled");
+} else {
+  localStorage.setItem("darkMode", "disabled");
+}
 
-  // Save the perference to LocalStorage
-  // if (document.body.classList.contains("dark-background")) {
-  //   localStorage.setItem("darkMode", "enabled");
-  // } else {
-  //   localStorage.setItem("darkMode", "disabled");
-  // }
+// ===== About paragragph switching =====
+const myselfLink = document.getElementById("myself-link");
+const educationLink = document.getElementById("education-link");
+const skillsLink = document.getElementById("skills-link");
+
+const aboutPara = document.getElementById("about-para");
+const educationPara = document.getElementById("education-para");
+const skillsPara = document.getElementById("skills-para");
+
+educationLink.addEventListener("click", () => {
+  aboutPara.style.display = "none";
+  educationPara.style.display = "block";
+});
+
+myselfLink.addEventListener("click", () => {
+  aboutPara.style.display = "block";
+  educationPara.style.display = "none";
+});
+
+skillsLink.addEventListener("click", () => {
+  aboutPara.style.display = "none";
+  educationPara.style.display = "none";
+  skillsPara.style.display = "flex";
 });
 
 const year = document.getElementById("year");
